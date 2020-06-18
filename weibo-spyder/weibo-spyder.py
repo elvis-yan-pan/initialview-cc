4c4502bf84487a4f24c4f6727ff530e1ad9f7417
Elvis-Pan <topanyan@sina.com>
2020-06-19 01:19:15 +0800

    added comments

--- weibo-spyder/weibo-spyder.py
+++ weibo-spyder/weibo-spyder.py
@@ -18,12 +18,14 @@ image = 'airplane.jpg' # the background of the word cloud
 
 def clean_text(text):
     # Get rid of special characters
-    dr = re.compile(r'(<)[^>]+>', re.S)
+    dr = re.compile(r'(<)[^>]+>', re.S) # <xxx>
     dd = dr.sub('', text)
-    dr = re.compile(r'#[^#]+#', re.S)
+    dr = re.compile(r'#[^#]+#', re.S)   # #xxx#
     dd = dr.sub('', dd)
-    dr = re.compile(r'@[^ ]+ ', re.S)
+    dr = re.compile(r'@[^ ]+ ', re.S)   # [xxx]
     dd = dr.sub('', dd)
+
+    # remove words in the rm_words list
     for w in rm_words:
         regex = '[' + w + ' ]+'
         dr = re.compile(regex)
@@ -37,8 +39,11 @@ def fetch_data(query_val, page_id):
     query_val: the keyword
     page_id: the page number
     """
+    # request and load json files
     resp = requests.get(url_template.format(query_val, query_val, page_id))
     card_group = json.loads(resp.text)['data']['cards'][0]['card_group']
+
+    # extract and store useful information 
     mblogs = []
     for card in card_group:
         mblog = card['mblog']
@@ -54,7 +59,7 @@ def fetch_data(query_val, page_id):
     return mblogs
 
 
-def remove_duplication(mblogs):
+def remove_duplicates(mblogs):
     assert(len(mblogs) > 0) # the mblogs should not be empty
     mid_set = {mblogs[0]['mid']}
     new_blogs = []
@@ -72,11 +77,15 @@ def fetch_pages(query_val, page_num):
             mblogs.extend(fetch_data(query_val, page_id))
         except Exception as e:
             print(e)
+    
+    # remove duplicates
+    mblogs = remove_duplicates(mblogs)
 
-    mblogs = remove_duplication(mblogs)
+    # save in local json file
     fp = open('result_{}.json'.format(query_val), 'w', encoding='utf-8')
     json.dump(mblogs, fp, ensure_ascii=False, indent=4)
     print("Result saved to result_{}.json".format(query_val))
+    return
 
 
 def get_keywords(mblogs):
@@ -88,10 +97,14 @@ def get_keywords(mblogs):
 
 
 def gen_img(texts, img_file, filename):
-    # Generate word cloud
+    # generate data
     data = ' '.join(text for text in texts)
+
+    # background for the word cloud
     image_coloring = Image.open(img_file)
     image_coloring = np.array(image_coloring)
+
+    # generate and save image 
     wc = WordCloud(
         background_color='white',
         mask=image_coloring,
@@ -99,14 +112,20 @@ def gen_img(texts, img_file, filename):
     )
     wc.generate(data)
     wc.to_file(filename + '_wordcloud.png')
+    return
 
 
 if __name__ == '__main__':
+    # fetch pages
     fetch_pages(keyword, 50)
+
+    # load data from the local json files
     mblogs = json.loads(open('result_{}.json'.format(keyword), 'r', encoding='utf-8').read())
     print('Number of Posts:', len(mblogs))
-    words = []
-    for blog in mblogs:
-        words.extend(jieba.analyse.extract_tags(blog['text']))
+
+    # extract words from the blogs
+    words = get_keywords(mblogs) 
     print("Word Count:", len(words))
+
+    # generate word cloud
     gen_img(words, image, keyword)
