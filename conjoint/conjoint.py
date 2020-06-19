32aea3e98a26ffd43bb36a914e080d3b81aa9ed7
Elvis-Pan <topanyan@sina.com>
2020-06-19 12:48:32 +0800

    added outline

--- conjoint/conjoint.py
+++ conjoint/conjoint.py
@@ -1,10 +1,40 @@
+# Conjoint Analysis for the InitialView Case Competition
+
 '''
-Author: Yan Pan
-Conjoint Analysis for the case competition
+General Notes:
+R respondents, T tasks, C concepts 
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
+
+filename = ''
+
+class ConjointModel():
+    def __init__(self):
+        '''
+        utilities: the values Ux of all the ideas of the concepts. 
+        '''
+
+    def read_data(self, filename):
+        # read data from file
+        pass
+
+
+    def get_probability(self):
+        # get the probability of choosing a concept
+        pass
+
+
+    def estimate(self):
+        # max likelihood
+        pass
+
+
+if __name__ == "__main__":
+    print("Conjoint Analysis")
+    Model = ConjointModel()
+    Model.read_data(filename)
+    Model.estimate()
 
