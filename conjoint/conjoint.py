6bee45b0bedd0cecd252e656b55322e33131a9fa
Elvis-Pan <topanyan@sina.com>
2020-06-21 15:52:24 +0800

    finished first version of conjoint analysis, main problem remaining: overflow

--- conjoint/conjoint.py
+++ conjoint/conjoint.py
@@ -2,64 +2,126 @@
 
 '''
 General Notes:
-R respondents, T tasks, C concepts 
+R respondents (unknown)
+T tasks (15)
+C concepts (3 or 4)
+4 attributes: theme color icon font
+Size of vector x and w: 4+4+4+4 = 16
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+# from sklearn.linear_model import LogisticRegression
+from scipy.optimize import minimize
+
+formatfile = 'format{}.csv'
+filename = 'result{}.csv'
 
 
-filename = ''
+def index_to_array(pic_index):
+    # index of 4 attributes
+    pic_index = pic_index - 1
+    attribute1 = int(pic_index / 64)
+    pic_index = pic_index % 64
+    attribute2 = int(pic_index / 16)
+    pic_index = pic_index % 16
+    attribute3 = int(pic_index / 4)
+    pic_index = pic_index % 4
+    attribute4 = pic_index
+    res = np.zeros(15)
+    res[attribute1] = 1
+    res[3 + attribute2] = 1
+    res[7 + attribute3] = 1
+    res[11 + attribute4] = 1
+    return res
+
 
 class ConjointModel():
     def __init__(self):
-        '''
-        utilities: the values Ux of all the ideas of the concepts. 
-        '''
+        """
+        utilities: the values Ux of all the ideas of the concepts.
+        """
+        self.num_total_attributes = 15
+        self.num_tasks = 32
+        self.num_concepts = 3
+        self.question = None
+        self.question_vec = None
+        self.ans = None
+        self.data = None
+        self.Model = None
+        self.res = None
+        self.question_mat = None
 
-    def read_data(self, filename):
-        # read data from file
-        pass
+    def read_questions(self, filename):
+        df = pd.read_csv(filename)
+        self.question = df.to_numpy()
+        print(self.question.shape)
+        return self.question
 
+    def build_question_vector(self):
+        qvec = [[] for j in range(self.question.shape[0])]
+        for i in range(self.question.shape[0]):
+            qvec[i] = [index_to_array(self.question[i][j])
+                       for j in range(self.question.shape[1])]
+        self.question_vec = np.array(qvec)
+        return self.question_vec
 
-    def get_probability(self):
-        # get the probability of choosing a concept
-        pass
+    def read_data(self, filename_format):
+        self.build_question_vector()
+        data_vec_all = []
+        data_index_all = []
+        question_vec = []
+        for i in range(4):
+            df = pd.read_csv(filename_format.format(i))
+            nda = df.to_numpy()
+            raw_data = np.array(nda[:, 8:40], dtype=np.int)
+            print(raw_data.shape)
+            data_vec = [[] for j in range(raw_data.shape[0])]
+            data_index = [[] for j in range(raw_data.shape[0])]
+            for j in range(raw_data.shape[0]):
+                question_vec.append(self.question_vec[i*32:(i+1)*32])
+                for k in range(raw_data.shape[1]):
+                    pic_index = self.question[i * 32 + j][raw_data[j][k] - 1]
+                    data_vec[j].append(index_to_array(pic_index))
+                    data_index[j].append(pic_index)
+            data_vec_all.extend(data_vec)
+            data_index_all.extend(data_index)
+        self.data = np.array(data_index_all)
+        self.ans = np.array(data_vec_all)
+#        self.ans = np.resize(
+#            self.ans,
+#            (self.ans.shape[0] * self.ans.shape[1], self.ans.shape[2])
+#        )  # resize the ans into 2 dimensional
+        self.question_mat = np.array(question_vec)
+        return self.ans
 
+    def log_likelihood(self, w):
+        s = 0
+        for i in range(self.ans.shape[0]):
+            for j in range(self.ans.shape[1]):
+                xrt = self.question_mat[i][j]
+                dots = np.array([np.dot(xrt[k], w) for k in range(self.num_concepts)])
+                exp_dots = np.exp2(dots)
+                sum_dots = np.log(exp_dots.sum())
+                s += dots.sum() / sum_dots
+        return -s
 
-    def estimate(self, estimator='max_likelihood'):
-        if estimator == 'max_likelihood':
-            self.max_likelihood()
-        elif estimator == 'logistic_regression':
-            self.logistic_regression()
-        else:
-            print("Invalid estimation method.")
-        return
-
-    
-    def max_likelihood(self):
-        pass
-
-
-    def logistic_regression(self):
-        pass
-
+    def estimate(self):
+        x0 = np.ones(self.num_total_attributes) / self.num_total_attributes
+        self.res = minimize(self.log_likelihood, x0, method='Nelder-Mead')
+        return self.res
 
     def generate_result(self):
         pass
-
 
     def generate_graph(self):
         pass
 
 
-
 if __name__ == "__main__":
     print("Conjoint Analysis")
     Model = ConjointModel()
-    Model.read_data(filename)
+    Model.read_questions('question.csv')
+    Model.read_data('data{}.csv')
     Model.estimate()
-    Model.generate_graph()
-
-
